989943fe3b21f09478f9ab3162b9f824bcba0438
Cody Brunner <rockchalkwushock@icloud.com>
2021-03-30 11:51:06 -0500

    goes through controller and view docs.

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    |> put_layout("admin.html")
+    |> render(:index)
   end
 end
